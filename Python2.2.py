c7b151818c290e3baeed57d8f1a519d8f88d5606
Nikita <nikdenpro@gmail.com>
2025-02-18 18:05:22 +0200

    First Commit

---
+++ Python2.2.py
@@ -0,0 +1,13 @@
+text = "i love python programming in python" # tehtava 1
+#Заменяю питон на Питон
+replaced_text = text.replace('python', 'Python')
+print(replaced_text)
+
+text1 = 'Hello World!' # tehtava 2
+# меняю заглавыне буквы и строчные между собой
+swapcased_text = text1.swapcase()
+print(swapcased_text)
+
+a = "Hello World!" # tehtava 3
+# использую index slicing и беру значение элемента по индексу
+print(a[6:11])
